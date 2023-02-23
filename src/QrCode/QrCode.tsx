213ffa2e4ce734e50c4a269631a92cb96831ae1f
tcmak217 <tcmak217@outlook.com>
2023-02-23 21:45:32 +0800

    feat: add mobile viewport implementation

--- src/QrCode/QrCode.tsx
+++ src/QrCode/QrCode.tsx
@@ -1,7 +1,29 @@
 import React from "react"
+import QrCodeImage from "../images/image-qr-code.png"
 
 function QrCode() {
-  return <div>QrCode</div>
+  return (
+    <div className="bg-light-gray h-screen w-full">
+      <div className="w-full h-full flex justify-center items-center">
+        <div className="w-5/6 h-[75%] min-h-[500px] bg-white rounded-3xl">
+          <div className="h-full flex flex-col">
+            <div className="w-full flex justify-center mt-4">
+              <div className="w-[90%] max-w-[280px]">
+                <img className="rounded-xl" src={QrCodeImage} alt="qr code" />
+              </div>
+            </div>
+            <div className="font-black text-dark-blue text-xl px-10 pt-6 text-center grow">
+              Improve your front-end skills by building projects
+            </div>
+            <div className="text-grayish-blue text-sm px-10 pt-4 text-center grow">
+              Scan the QR code to visit Frontend Mentor and take your coding skills to the next
+              level
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default QrCode
