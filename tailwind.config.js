d71edd47a967f5e3620779ceb4f245db8e5d340c
tcmak217 <tcmak217@outlook.com>
2023-02-23 19:49:43 +0800

    feat: add custom font family, font size and colors to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: "15px",
+      fontFamily: {
+        sans: ["Outfit", "sans-serif"],
+      },
+      colors: {
+        white: "hsl(0, 0%, 100%)",
+        "light-gray": "hsl(212, 45%, 89%)",
+        "grayish-blue": "hsl(220, 15%, 55%)",
+        "dark-blue": "hsl(218, 44%, 22%)",
+      },
+    },
   },
   plugins: [],
 }
